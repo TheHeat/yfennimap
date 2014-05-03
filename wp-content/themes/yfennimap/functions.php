2fd24bcbf385982c79b711e9ec2111fee36d174e
ben-boulders <ben@benjones-e.com>
2014-05-03 14:30:38 +0100

    Facebook SDK folder

--- wp-content/themes/yfennimap/functions.php
+++ wp-content/themes/yfennimap/functions.php
@@ -135,3 +135,5 @@ require_once( get_template_directory() .'/src/Facebook/FacebookSDKException.php'
 require_once( get_template_directory() .'/src/Facebook/FacebookRequestException.php' );
 require_once( get_template_directory() .'/src/Facebook/FacebookAuthorizationException.php' );
 require_once( get_template_directory() .'/src/Facebook/GraphObject.php' );
+
+
