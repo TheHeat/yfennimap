ec5f5196acfbdf585194b6ca96f643a3d4e55a27
ben-boulders <ben@benjones-e.com>
2014-05-05 16:54:27 +0100

    Message Box to post to Facebook

--- wp-content/themes/yfennimap/index.php
+++ wp-content/themes/yfennimap/index.php
@@ -12,13 +12,26 @@
  */
 
 get_header();
+?>
 
-$token = 'CAADWmmqwixABAB9ZC1SQHdX8AkGknsY3OxB2zY2ljiZByslb064vVg9j2YRgZA3ME8MEauTUWxW49pYZAgoDDA3DZAPaOPDS31ZCZAV0bDlpAlkotKUBNkzKVXzZAZAy2DUdQw9O8WwFB8NhWSAZCp5pzu1o7KNG6XmXxQbGu2vh0vJtRJ4lT42mbxQJeZAhDuqnnUZD';
-$edge = 'feed';
-$content = 'something else';
+<form>
+  <label for"message">Message to Post</label>
+  <input name="message" class="message" id="message" type="text">
+  <input type="submit" value="submit">
+</form>
 
-$fb = fb_post_on_page($token, $edge, $content);
+<?php
 
-echo $fb;
+if(null!=($_GET["message"])):
+
+  $token = 'CAADWmmqwixABAB9ZC1SQHdX8AkGknsY3OxB2zY2ljiZByslb064vVg9j2YRgZA3ME8MEauTUWxW49pYZAgoDDA3DZAPaOPDS31ZCZAV0bDlpAlkotKUBNkzKVXzZAZAy2DUdQw9O8WwFB8NhWSAZCp5pzu1o7KNG6XmXxQbGu2vh0vJtRJ4lT42mbxQJeZAhDuqnnUZD';
+  $edge = 'feed';
+  $content = $_GET["message"];
+
+  $fb = fb_post_on_page($token, $edge, $content);
+
+  echo $fb;
+
+endif;
 
 get_footer(); ?>
