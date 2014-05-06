002aacdcc772881a4026373b3fcb89482f532d73
Marc Heatley <marc@marcheatleydesign.com>
2014-05-06 15:12:59 +0100

    Map template PHP

---
+++ wp-content/themes/yfennimap/map.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Map
+ *
+ *
+ * Map interface for "pins" custom post type
+ *
+ *
+ * @package yfennimap
+ */
+
+get_header();
+?>
+<div id="map-canvas"/>
+
+<?php get_footer(); ?>
+	
