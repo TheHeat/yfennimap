19f23b1370102615c4a6241610cebaf3d037c6ea
Marc Heatley <marc@marcheatleydesign.com>
2014-05-07 01:04:31 +0100

    Tidy up geocode function

--- wp-content/themes/yfennimap/js/map.js
+++ wp-content/themes/yfennimap/js/map.js
@@ -1,38 +1,31 @@
 // Define map in the Global scope
-var map;
+var geocoder = new google.maps.Geocoder();
 
+// Initialize the map
 function initialize() {
-	Geocoder = new google.maps.Geocoder();
+	
 	var mapOptions = {
 		center: new google.maps.LatLng(51.825366,-3.019423),
-		zoom: 12
+		zoom: 6,
+		disableDefaultUI: true,
     };
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 
-function coordAddress(addressString) {
-	Geocoder.geocode( { 'address': addressString}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-				console.log(results[0].geometry.location);
-    	}
-  });
-}
 
-
+// Funstion to convert a location address to a geocoded object, centre the map and fill the viewport with the location
 function codeAddress(addressString) {
-	Geocoder.geocode( { 'address': addressString}, function(results, status) {
+	geocoder.geocode( { 'address': addressString}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-			map.setCenter(results[0].geometry.location);
+			console.log(results[0]);
 
-			var marker = new google.maps.Marker({
-				map: map,
-				position: results[0].geometry.location
-			});
+			map.setCenter(results[0].geometry.location);
+			map.fitBounds(results[0].geometry.viewport);
+
     	} else {
     		alert('Geocode was not successful: ' + status);
     	}
   });
 }
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
