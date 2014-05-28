fea1a5e7dba7cf1ce788f21d61a5aa6d54bd1e98
Andrew Shankie <andrew@andrewshankie.com>
2014-05-28 15:16:39 +0100

    Update page token

--- wp-content/themes/yfennimap/inc/fb-login.php
+++ wp-content/themes/yfennimap/inc/fb-login.php
@@ -29,7 +29,7 @@ use Facebook\FacebookOtherException;
 define('page_id', '276813939159864');
 define('app_id', '235958703262480');
 define('app_secret', 'f608ec2687f60c051396c4d0fabaae06');
-define('page_token', 'CAADWmmqwixABAMCTlPK1FrjU1u4ZBZAZB96QVy11bZBCYx7JDU91RwEVPZAQZCwJx9VBIEj9sm1mGePcZAWeilZBNUzRk5bDjZBkbd9EHAUDrd2VHKpwcc3nq1fgFjzykEKnMbzjFjfpUPAHSSKaVf39RMJHGibEvsXnEtnzR6BzDa78Io2MutZA7VaMBmpgPE3F4ZD');
+define('page_token', 'CAACEdEose0cBANaK98nam9yUhhSARC6HtLYz9ilrmWNZAftyP2UAZBZC1YmXlgEYgVyUVte1BdUC7UdBljWoZAif6HHs1ZCVqwS8J95qA5CQGdzqELrPql2KNWnZCONsv9zOxXoI6gVvDdIZAmDpRxZBqs5lfpPLQSlB5Q3jQjarXuSOGW34VF3fZC83YbaejVg5iVphNqixL6gZDZD');
 
 
 function fb_login(){
@@ -58,7 +58,10 @@ function fb_login(){
 		//create a new FB session
 
 		try {
-		  $session = $helper->getSessionFromRedirect();
+
+			//get the session from the query string
+			$session = $helper->getSessionFromRedirect();
+
 		} catch( FacebookRequestException $ex ) {
 		  // When Facebook returns an error
 		} catch( Exception $ex ) {
@@ -123,7 +126,7 @@ function fb_login(){
 				<?php 
 
 				// show login url
-    			echo '<a class="tool" href="' . $helper->getLoginUrl($params) . '">Login</a>';
+    			echo '<a href="' . $helper->getLoginUrl($params) . '">Login</a>';
 
     			?>
 
