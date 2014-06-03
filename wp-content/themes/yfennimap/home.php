7c55316f14e2f6ef6cb3a439e3d4dc6a1f42d12f
Marc Heatley <marc@marcheatleydesign.com>
2014-06-03 09:21:51 +0100

    move fb_get_token conditional outside the toolbox

--- wp-content/themes/yfennimap/home.php
+++ wp-content/themes/yfennimap/home.php
@@ -13,24 +13,24 @@ get_header();
 
 <?php get_template_part('map-pins' ); ?>
 
+<?php if(fb_get_token()): ?>
+
 <div class="toolbox">
 	<div class="actions" style="display:none;"></div>
 	<div class="handle" tabindex="1">Add a pin</div>
 	<div class="box">
-	  	<?php if(fb_get_token()): ?>
+	  	
 		    <a href="#" data-media="link" class="tool add link">Web Link</a>
 		    <a href="#" data-media="video" class="tool add video">Video</a>
 		    <a href="#" data-media="pictures" class="tool add pictures">Pictures</a>
 		    <a href="#" data-media="text" class="tool add words">Words</a>
-		<?php else: ?>
-			<?php fb_login() ;?>
-		<?php endif; ?>
   	</div>
+<?php endif; ?>
 </div>
 <div id="map-canvas">
 </div>
 <div id="media-modal">
-</div>
 
+</div>
 <?php get_footer(); ?>
 	
