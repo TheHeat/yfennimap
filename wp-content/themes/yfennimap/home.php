f6343c2a516e63d20f4750a2deef226a1548132e
TheHeat <marc@marcheatleydesign.com>
2014-06-09 16:30:53 +0100

    Make toolbox conditional on fb_get_token();

--- wp-content/themes/yfennimap/home.php
+++ wp-content/themes/yfennimap/home.php
@@ -11,19 +11,23 @@
 get_header();
 ?>
 
-<?php get_template_part('map-pins' ); ?>
+<?php get_template_part('map-pins' ); ?>	
 
-<div class="toolbox">
-	<div class="actions" style="display:none;"></div>
-	<div class="handle" tabindex="1">Add a pin</div>
-	<div class="box">
-	  	
-		    <a href="#" data-media="link" class="tool add link">Web Link</a>
-		    <a href="#" data-media="video" class="tool add video">Video</a>
-		    <a href="#" data-media="pictures" class="tool add pictures">Pictures</a>
-		    <a href="#" data-media="text" class="tool add words">Words</a>
-  	</div>
-</div>
+<?php if(fb_get_token()): ?>
+	<div class="toolbox">
+
+		
+		<div class="actions" style="display:none;"></div>
+		<div class="handle" tabindex="1">Add a pin</div>
+		<div class="box">
+		  	
+			    <a href="#" data-media="link" class="tool add link">Web Link</a>
+			    <a href="#" data-media="video" class="tool add video">Video</a>
+			    <a href="#" data-media="pictures" class="tool add pictures">Pictures</a>
+			    <a href="#" data-media="text" class="tool add words">Words</a>
+	  	</div>
+	</div>
+<?php endif; ?>
 <div id="map-canvas">
 </div>
 <div id="media-modal">
