0d316cea3271975c3fece09954909365de96f6d6
ben-boulders <ben@benjones-e.com>
2014-06-24 11:26:43 +0100

    working sync

--- wp-content/themes/yfennimap/single-pin.php
+++ wp-content/themes/yfennimap/single-pin.php
@@ -19,18 +19,15 @@ $fb_media = new FB_Media($post->ID);
 
 echo '<pre>'; print_r($fb_media); echo '</pre>'; 
 
-echo $fb_media-> get_url();
+$mediaType = get_field('media_type');
+
+pinDisplay($mediaType);
+
+// echo $fb_media-> get_url();
 // echo $fb_media-> get_long_text();
 // echo $fb_media-> get_images();
 // echo $fb_media-> get_video();
 // echo $fb_media-> get_comments();
 // echo $fb_media-> get_link();
 
-?>
-
-https://www.facebook.com/permalink.php?story_fbid=288486637992594&id=276813939159864
-
-<?php
-
-echo '</div>';
 get_footer(); ?>
